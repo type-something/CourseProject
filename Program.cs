1ead0d904f147e2f1f8bbe0dd15c8b89216e93b5
TropicLuv <noname@fakegmail.com>
2025-05-30 00:45:51 +0400

    feat: implement basic TcpListener. added webroot dir

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net;
+using System.Net.Sockets;
+
+const int PORT = 8080;
+
+async Task HandleClientAsync(TcpClient client)
+{
+    Console.WriteLine("Client connected");
+    await Task.CompletedTask;
+}
+
+var listener = new TcpListener(IPAddress.Any, PORT);
+listener.Start();
+Console.WriteLine($"Server listening on port {PORT}");
+
+
+
+while (true)
+{
+    var client = await listener.AcceptTcpClientAsync();
+    _ = Task.Run(() => HandleClientAsync(client));
+}
