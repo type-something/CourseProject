fcd5ccca6fa1e0ae26977191222493370149c0cc
TropicLuv <noname@fakegmail.com>
2025-06-01 20:38:29 +0400

    fix: README.md rework feat: added unit tests

---
+++ CourseProject/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using CourseProject.Configuration;
+using CourseProject.Services;
+
+var config = new ServerConfig();
+var fileService = new FileService(config.WebRootPath);
+var loggingService = new LoggingService();
+var httpHandler = new HttpHandler(fileService, loggingService);
+
+var poolMonitor = new Timer(_ =>
+{
+    // Console.WriteLine("here");
+    loggingService.LogThreadPoolStatus();
+}, null, TimeSpan.Zero, TimeSpan.FromSeconds(1));
+
+var listener = new TcpListener(IPAddress.Any, config.Port);
+listener.Start();
+Console.WriteLine($"Server on {config.Port}, root {Path.GetFullPath(config.WebRootPath)}");
+loggingService.LogOsThreads("startup");
+
+while (true)
+{
+    var client = await listener.AcceptTcpClientAsync();
+    _ = Task.Run(() => httpHandler.HandleClientAsync(client));
+}
